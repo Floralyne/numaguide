0196ffb6c3917f3bc6bf044484a4eeebc41a0734
ameliegauthier <99548225+ameliegauthier@users.noreply.github.com>
2022-05-18 14:18:00 +0200

    slides php

---
+++ wp-content/themes/numaguide/slides/Slides Amélie/slide_19.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Template Name: Slide 19
+ * Template Post Type: post, page
+ */
+
+?>
+<?php get_header() ?>
+
+
+<section id="slide_19">
+    <div id="slide_19_c" class="container-fluid">
+        <div id="slide_19_r" class="row vh-100">
+            <div id="slide_19" class="col text-center m-auto">
+                <img id="slide_19_image" class="img-fluid" src="">
+            </div>
+        </div>
+    </div>
+</section>
---
+++ wp-content/themes/numaguide/slides/Slides Amélie/slide_8.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Template Name: Slide 19
+ * Template Post Type: post, page
+ */
+
+?>
+<?php get_header() ?>
+
+<section id="slide_8">
+        <div id="slide_8_c" class="container-fluid">
+            <div id="slide_8_r" class="row vh-100">
+                <div id="slide_8_div" class="col text-center mt-auto mb-auto p-5">
+                    <div id="slide_8_titre_principal" class="slide_8_titre">“Je mourrai quand je voudrai mourir. Mais,
+                        alors je mourrai avec quelqu’un. Mourir seul, c’est trop ennuyeux.”</div>
+                    <div id="slide_8_sous_titre" class="slide_8_sous_titre">“L'art est une sottise. ”</div>
+                    <div id="slide_8_auteur" class="slide_8_auteur">Jacques Vaché</div>
+                </div>
+            </div>
+        </div>
+</section>
