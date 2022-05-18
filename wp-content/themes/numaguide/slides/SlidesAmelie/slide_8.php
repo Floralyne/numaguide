198f1e72d00595fc7ac338c9095de98700860c99
ameliegauthier <99548225+ameliegauthier@users.noreply.github.com>
2022-05-18 16:19:03 +0200

    Update slide_8.php

--- wp-content/themes/numaguide/slides/SlidesAmelie/slide_8.php
+++ wp-content/themes/numaguide/slides/SlidesAmelie/slide_8.php
@@ -12,10 +12,66 @@
         <div id="slide_8_c" class="container-fluid">
             <div id="slide_8_r" class="row vh-100">
                 <div id="slide_8_div" class="col text-center mt-auto mb-auto p-5">
-                    <div id="slide_8_titre_principal" class="slide_8_titre">“Je mourrai quand je voudrai mourir. Mais,
-                        alors je mourrai avec quelqu’un. Mourir seul, c’est trop ennuyeux.”</div>
-                    <div id="slide_8_sous_titre" class="slide_8_sous_titre">“L'art est une sottise. ”</div>
-                    <div id="slide_8_auteur" class="slide_8_auteur">Jacques Vaché</div>
+                    <div id="slide_8_titre_principal" class="slide_8_titre"> <?php
+
+//die(var_dump($post));
+//$post = get_post($args);
+//die(var_dump($post));
+//die(var_dump($args['article']));
+
+//$post_tab = $args['article'];
+//$post = $post_tab[0];
+//die(var_dump($post));
+//die(var_dump($post1->post_content));
+$blocks = parse_blocks($post->post_content);
+
+//die(var_dump($blocks));
+foreach ($blocks as $block) {
+    if ($block['blockName'] == 'core/paragraph') {
+        echo render_block($block);
+    }
+}
+?></div>
+                    <div id="slide_8_sous_titre" class="slide_8_sous_titre"> <?php
+
+//die(var_dump($post));
+//$post = get_post($args);
+//die(var_dump($post));
+//die(var_dump($args['article']));
+
+//$post_tab = $args['article'];
+//$post = $post_tab[0];
+//die(var_dump($post));
+//die(var_dump($post1->post_content));
+$blocks = parse_blocks($post->post_content);
+
+//die(var_dump($blocks));
+foreach ($blocks as $block) {
+    if ($block['blockName'] == 'core/paragraph') {
+        echo render_block($block);
+    }
+}
+?></div>
+                    <div id="slide_8_auteur" class="slide_8_auteur"> <?php
+
+//die(var_dump($post));
+//$post = get_post($args);
+//die(var_dump($post));
+//die(var_dump($args['article']));
+
+//$post_tab = $args['article'];
+//$post = $post_tab[0];
+//die(var_dump($post));
+//die(var_dump($post1->post_content));
+$blocks = parse_blocks($post->post_content);
+
+//die(var_dump($blocks));
+foreach ($blocks as $block) {
+    if ($block['blockName'] == 'core/paragraph') {
+        echo render_block($block);
+    }
+}
+?></div>
                 </div>
             </div>
         </div>
