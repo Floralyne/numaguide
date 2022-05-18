c7f531043f1d23d616c1f11af70e1e1490b8ba4d
ameliegauthier <99548225+ameliegauthier@users.noreply.github.com>
2022-05-18 18:23:40 +0200

    404

--- wp-content/themes/numaguide/404.php
+++ wp-content/themes/numaguide/404.php
@@ -15,7 +15,7 @@
 <section class="page404">
     <div class="container404">
         <div class="image404">
-            <img src="<?= get_template_directory_uri() . '/assets/images/404shadow3.svg'; ?>">
+            <img src="<?= get_template_directory_uri() . '/assets/images/404.svg'; ?>">
         </div>
         <div class="message404">La page que vous recherchez n'existe pas !</div>
     </div>
