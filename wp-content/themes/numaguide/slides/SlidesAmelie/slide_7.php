cca12378b64aaca3ad9826c1494ce10f3218e041
ameliegauthier <99548225+ameliegauthier@users.noreply.github.com>
2022-05-18 16:17:23 +0200

    slide 7

---
+++ wp-content/themes/numaguide/slides/SlidesAmelie/slide_7.php
@@ -0,0 +1,138 @@
+<?php
+
+/**
+ * Template Name: Slide 7
+ * Template Post Type: post, page
+ */
+
+?>
+<?php get_header() ?>
+
+<section id="slide_7">
+        <div id="slide_7_c" class="container-fluid">
+            <div id="slide_7_r" class="row vh-100">
+                <div id="slide_7_div" class="col text-center mt-auto mb-auto p-5">
+                    <div id="slide_7_bib" class="slide_7_bib"> <?php
+
+//die(var_dump($post));
+//$post = get_post($args);
+//die(var_dump($post));
+//die(var_dump($args['article']));
+
+//$post_tab = $args['article'];
+//$post = $post_tab[0];
+//die(var_dump($post));
+//die(var_dump($post1->post_content));
+$blocks = parse_blocks($post->post_content);
+
+//die(var_dump($blocks));
+foreach ($blocks as $block) {
+    if ($block['blockName'] == 'core/paragraph') {
+        echo render_block($block);
+    }
+}
+?></div>
+                    <div id="slide_7_livre_1" class="slide_7_livre"> <?php
+
+//die(var_dump($post));
+//$post = get_post($args);
+//die(var_dump($post));
+//die(var_dump($args['article']));
+
+//$post_tab = $args['article'];
+//$post = $post_tab[0];
+//die(var_dump($post));
+//die(var_dump($post1->post_content));
+$blocks = parse_blocks($post->post_content);
+
+//die(var_dump($blocks));
+foreach ($blocks as $block) {
+    if ($block['blockName'] == 'core/paragraph') {
+        echo render_block($block);
+    }
+}
+?></div>
+                    <div id="slide_7_livre_2" class="slide_7_livre"> <?php
+
+//die(var_dump($post));
+//$post = get_post($args);
+//die(var_dump($post));
+//die(var_dump($args['article']));
+
+//$post_tab = $args['article'];
+//$post = $post_tab[0];
+//die(var_dump($post));
+//die(var_dump($post1->post_content));
+$blocks = parse_blocks($post->post_content);
+
+//die(var_dump($blocks));
+foreach ($blocks as $block) {
+    if ($block['blockName'] == 'core/paragraph') {
+        echo render_block($block);
+    }
+}
+?></div>
+                    <div id="slide_7_livre_3" class="slide_7_livre"> <?php
+
+//die(var_dump($post));
+//$post = get_post($args);
+//die(var_dump($post));
+//die(var_dump($args['article']));
+
+//$post_tab = $args['article'];
+//$post = $post_tab[0];
+//die(var_dump($post));
+//die(var_dump($post1->post_content));
+$blocks = parse_blocks($post->post_content);
+
+//die(var_dump($blocks));
+foreach ($blocks as $block) {
+    if ($block['blockName'] == 'core/paragraph') {
+        echo render_block($block);
+    }
+}
+?></div>
+                    <div id="slide_7_livre_4" class="slide_7_livre"> <?php
+
+//die(var_dump($post));
+//$post = get_post($args);
+//die(var_dump($post));
+//die(var_dump($args['article']));
+
+//$post_tab = $args['article'];
+//$post = $post_tab[0];
+//die(var_dump($post));
+//die(var_dump($post1->post_content));
+$blocks = parse_blocks($post->post_content);
+
+//die(var_dump($blocks));
+foreach ($blocks as $block) {
+    if ($block['blockName'] == 'core/paragraph') {
+        echo render_block($block);
+    }
+}
+?></div>
+                    <div id="slide_7_livre_5" class="slide_7_livre"> <?php
+
+//die(var_dump($post));
+//$post = get_post($args);
+//die(var_dump($post));
+//die(var_dump($args['article']));
+
+//$post_tab = $args['article'];
+//$post = $post_tab[0];
+//die(var_dump($post));
+//die(var_dump($post1->post_content));
+$blocks = parse_blocks($post->post_content);
+
+//die(var_dump($blocks));
+foreach ($blocks as $block) {
+    if ($block['blockName'] == 'core/paragraph') {
+        echo render_block($block);
+    }
+}
+?></div>
+                </div>
+            </div>
+        </div>
+</section>
