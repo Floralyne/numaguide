5cbcd0702f3dc9c9591d59a92a0fd5b15ab3724f
ameliegauthier <99548225+ameliegauthier@users.noreply.github.com>
2022-05-19 10:55:20 +0200

    Update slide_7.php

--- wp-content/themes/numaguide/slides/slide_7.php
+++ wp-content/themes/numaguide/slides/slide_7.php
@@ -13,106 +13,36 @@
             <div id="slide_7_r" class="row vh-100">
                 <div id="slide_7_div" class="col text-center mt-auto mb-auto p-5">
                     <div id="slide_7_bib" class="slide_7_bib">Bibliographie</div>
-                    <div id="slide_7_livre_1" class="slide_7_livre"> <?php
+                    <div id="slide_7_livre_1" class="slide_7_livre"> 
+                    <?php
+                        $post_tab = $args['article'];
+                        $post = $post_tab[0];
+                        $blocks = parse_blocks($post->post_content);
 
-//die(var_dump($post));
-//$post = get_post($args);
-//die(var_dump($post));
-//die(var_dump($args['article']));
-
-//$post_tab = $args['article'];
-//$post = $post_tab[0];
-//die(var_dump($post));
-//die(var_dump($post1->post_content));
-$blocks = parse_blocks($post->post_content);
-
-//die(var_dump($blocks));
-foreach ($blocks as $block) {
-    if ($block['blockName'] == 'core/paragraph') {
-        echo render_block($block);
-    }
-}
-?></div>
-                    <div id="slide_7_livre_2" class="slide_7_livre"> <?php
-
-//die(var_dump($post));
-//$post = get_post($args);
-//die(var_dump($post));
-//die(var_dump($args['article']));
-
-//$post_tab = $args['article'];
-//$post = $post_tab[0];
-//die(var_dump($post));
-//die(var_dump($post1->post_content));
-$blocks = parse_blocks($post->post_content);
-
-//die(var_dump($blocks));
-foreach ($blocks as $block) {
-    if ($block['blockName'] == 'core/paragraph') {
-        echo render_block($block);
-    }
-}
-?></div>
-                    <div id="slide_7_livre_3" class="slide_7_livre"> <?php
-
-//die(var_dump($post));
-//$post = get_post($args);
-//die(var_dump($post));
-//die(var_dump($args['article']));
-
-//$post_tab = $args['article'];
-//$post = $post_tab[0];
-//die(var_dump($post));
-//die(var_dump($post1->post_content));
-$blocks = parse_blocks($post->post_content);
-
-//die(var_dump($blocks));
-foreach ($blocks as $block) {
-    if ($block['blockName'] == 'core/paragraph') {
-        echo render_block($block);
-    }
-}
-?></div>
-                    <div id="slide_7_livre_4" class="slide_7_livre"> <?php
-
-//die(var_dump($post));
-//$post = get_post($args);
-//die(var_dump($post));
-//die(var_dump($args['article']));
-
-//$post_tab = $args['article'];
-//$post = $post_tab[0];
-//die(var_dump($post));
-//die(var_dump($post1->post_content));
-$blocks = parse_blocks($post->post_content);
-
-//die(var_dump($blocks));
-foreach ($blocks as $block) {
-    if ($block['blockName'] == 'core/paragraph') {
-        echo render_block($block);
-    }
-}
-?></div>
-                    <div id="slide_7_livre_5" class="slide_7_livre"> <?php
-
-//die(var_dump($post));
-//$post = get_post($args);
-//die(var_dump($post));
-//die(var_dump($args['article']));
-
-//$post_tab = $args['article'];
-//$post = $post_tab[0];
-//die(var_dump($post));
-//die(var_dump($post1->post_content));
-$blocks = parse_blocks($post->post_content);
-
-//die(var_dump($blocks));
-foreach ($blocks as $block) {
-    if ($block['blockName'] == 'core/paragraph') {
-        echo render_block($block);
-    }
-}
-?></div>
+                        $paragraphes = array();
+                        foreach ($blocks as $block) {
+                            if ($block['blockName'] == 'core/paragraph') {
+                                $paragraphes[] = $block;
+                            }
+                        }
+                        <die var_dump>
+                        echo render_block($paragraphes[0]);
+?>
+                    </div>
+                    <div id="slide_7_livre_2" class="slide_7_livre">
+                    <?php
+                        echo render_block($paragraphes[1]);
+?>  
+                    </div>
+                    <div id="slide_7_livre_3" class="slide_7_livre"><?php
+                        echo render_block($paragraphes[2]);
+?>  </div>
+                    <div id="slide_7_livre_4" class="slide_7_livre"><?php
+                        echo render_block($paragraphes[3]);
+?>  </div>
+                    <div id="slide_7_livre_5" class="slide_7_livre"><?php
+                        echo render_block($paragraphes[4]);
+?>  </div>
                 </div>
             </div>
         </div>
