9fa39183b90f4749708bdf348e111a493663733c
FS <floralyne_segaud@yahoo.fr>
2022-05-19 10:45:15 +0200

    Slide 17 code PHP
    Mise au propre et fonctionnel quand plusieurs bloque de texte

--- wp-content/themes/numaguide/slides/slide_17.php
+++ wp-content/themes/numaguide/slides/slide_17.php
@@ -13,53 +13,25 @@
                 <div id="slide_17_div1" class="col text-justify mt-auto mb-auto p-5">
                     <div id="slide_17_texte1" class="slide_17_texte">
                     <?php
+                        $post_tab = $args['article'];
+                        $post = $post_tab[0];
+                        $blocks = parse_blocks($post->post_content);
 
-//die(var_dump($post));
-//$post = get_post($args);
-//die(var_dump($post));
-//die(var_dump($args['article']));
-
-$post_tab = $args['article'];
-$post = $post_tab[0];
-//die(var_dump($post));
-//die(var_dump($post1->post_content));
-$blocks = parse_blocks($post->post_content);
-
-//die(var_dump($blocks));
-$paragraphes = array();
-foreach ($blocks as $block) {
-    if ($block['blockName'] == 'core/paragraph') {
-        $paragraphes[] = $block;
-    }
-}
-echo render_block($paragraphes[0]);
+                        $paragraphes = array();
+                        foreach ($blocks as $block) {
+                            if ($block['blockName'] == 'core/paragraph') {
+                                $paragraphes[] = $block;
+                            }
+                        }
+                        echo render_block($paragraphes[0]);
 ?>
                     </div>
                 </div>
                 <div class="col text-justify mt-auto mb-auto p-5">
                     <div id="slide_17_texte2" class="slide_17_texte">
                     <?php
-
-//die(var_dump($post));
-//$post = get_post($args);
-//die(var_dump($post));
-//die(var_dump($args['article']));
-
-$post_tab = $args['article'];
-$post = $post_tab[0];
-//die(var_dump($post));
-//die(var_dump($post1->post_content));
-$blocks = parse_blocks($post->post_content);
-
-//die(var_dump($blocks));
-$paragraphes = array();
-foreach ($blocks as $block) {
-    if ($block['blockName'] == 'core/paragraph') {
-        $paragraphes[] = $block;
-    }
-}
-echo render_block($paragraphes[1]);
-?>                </div>
+                        echo render_block($paragraphes[1]);
+?>                </div>)
             </div>
         </div>
         </div>
