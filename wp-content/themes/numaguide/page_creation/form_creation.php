6b924a078abf1ff4b3ff21ead105f57cc0a88c0d
victoiremuller <99548262+victoiremuller@users.noreply.github.com>
2022-05-20 17:54:40 +0200

    Bouton de repli

--- wp-content/themes/numaguide/page_creation/form_creation.php
+++ wp-content/themes/numaguide/page_creation/form_creation.php
@@ -1,5 +1,3 @@
-
-
 <!Doctype html>
 <html>
 
@@ -33,11 +31,11 @@
 
 <body>
 
-<!-- formulaire de création de guide  -->
+    <!-- formulaire de création de guide  -->
     <div class="container">
 
 
-<!-- formulaire de renseignement des informations générales du guide -->
+        <!-- formulaire de renseignement des informations générales du guide -->
         <h2 class="pt-5 pb-4">Informations du guide</h2>
 
         <form>
@@ -63,7 +61,7 @@
             </div>
 
 
-<!-- formulaire de création des sections et slides  -->
+            <!-- formulaire de création des sections et slides  -->
             <h2 class="pt-5 pb-sm-4">Création du guide</h2>
 
             <h3 class="pb-sm-4">Thèmes des couleurs</h3>
@@ -77,6 +75,7 @@
                 </div>
 
             </div>
+<<<<<<< Updated upstream
     
             <div class="container-section" id="container-section">
                 <div class="row">
@@ -137,213 +136,58 @@
                     <input type="text" class="form-control" name="nom de la section" required>
                 </div>
                 <h4>Slide</h4>
+=======
+>>>>>>> Stashed changes
 
-                <div class="form-group pb-sm-2">
-                    <input type="url" class="form-control" name="video" placeholder="URL de la vidéo">
-                </div>
-                <h4>Slide</h4>
+            <div class="container-section" id="container-section">
+                <details>
+                    <summary>Section</summary>
+                        <hr>
+                        <div id="section_exemple">
+                            <div class="form-group pb-sm-2">
+                                <label for="nom_section1">Nom de la section : </label>
+                                <input type="text" class="form-control" name="nom de la section" required>
+                            </div>
 
-                <div class="form-group pb-sm-2">
-                    <input name="video" class="form-control" placeholder="URL de la vidéo" id="a26">
-                </div>
-                <div class="form-group pb-sm-2">
-                    <button type="button" class="btn btn-light btn-lg btn-block">Ajouter une slide</button>
-                </div>
 
-                <div class="form-group pb-sm-2">
-                    <button type="button" class="btn btn-light btn-lg btn-block">Aperçu</button>
-                    <button type="button" class="btn btn-light btn-lg btn-block">Valider</button>
-                </div>
+                            <h4>Slide</h4>
+
+                            <div class="form-group pb-sm-2">
+                                <textarea style="width: 700px; height: 200px; font-size: 15px;"
+                                    placeholder="Entrer du texte (Max. 500)" maxlength="500" wrap="soft"
+                                    class="form-control"></textarea>
+                                <input type="file" name="parcourir" placeholder="Parcourir (PNG, JPEG, GIF..)"
+                                    class="parcourir">
+                            </div>
+
+
+                            <div class="form-group pb-sm-2">
+                                <button type="button" class="btn btn-light btn-lg btn-block">Ajouter une slide</button>
+                            </div>
+
+                            <div class="form-group pb-sm-2">
+                                <button type="button" class="btn btn-light btn-lg btn-block">Aperçu</button>
+                                <button type="button" class="btn btn-light btn-lg btn-block">Valider</button>
+                            </div>
+                        </div>
+                </details>
             </div>
 
-
-            <div id="section_exemple">
-                <div class="row">
-                    <div class="col-sm">
-                        <h3>Section</h3>
-                    </div>
-                    <div class="col-sm">
-                        <a class="btn btn-dark float-right" data-toggle="collapse" href="#section2" role="button"
-                            aria-expanded="false" aria-controls="section2">Replier la section</a>
-                    </div>
-
-                </div>
-
-                <div class="collapse">
-                    <div class="form-group pb-sm-2">
-                        <label for="nom_section1">Nom de la section : </label>
-                        <input type="text" class="form-control" name="nom de la section" required>
-                    </div>
-
-                    <h4>Slide</h4>
-
-                    <div class="form-group pb-sm-2">
-                        <textarea style="width: 700px; height: 200px; font-size: 15px;"
-                            placeholder="Entrer du texte (Max. 500)" maxlength="500" wrap="soft"
-                            class="form-control"></textarea>
-                        <input type="file" name="parcourir" placeholder="Parcourir (PNG, JPEG, GIF..)"
-                            class="parcourir">
-                    </div>
-
-
-                    <div class="form-group pb-sm-2">
-                        <button type="button" class="btn btn-light btn-lg btn-block">Ajouter une slide</button>
-                    </div>
-
-                    <div class="form-group pb-sm-2">
-                        <button type="button" class="btn btn-light btn-lg btn-block">Aperçu</button>
-                        <button type="button" class="btn btn-light btn-lg btn-block">Valider</button>
-                    </div>
-                </div>
-            </div> -->
-
-
             <div class="form-group pb-sm-2">
-                <button type="button" class="btn btn-light btn-lg btn-block btn-add" id="supprimer_section">Supprimer une
+                <button type="button" class="btn btn-light btn-lg btn-block btn-add" id="supprimer_section">Supprimer
+                    une
                     section</button>
                 <button type="button" class="btn btn-light btn-lg btn-block btn-add" id="ajout_section">Ajouter une
                     section</button>
+                    <button type="button" class="btn btn-light btn-lg btn-block btn-add" id="ajout_section">CREER LE GUIDE</button>
             </div>
 
 
-        </form>
+    </div>
+
+
+    </form>
 
 </body>
 
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- Ancien code, a priori désuet, mais au cas où :  -->
-
-<!-- <!Doctype html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-
-    <script src="formulaires.js"></script>
-
-    <title>Création de guides</title>
-
-    <link rel="stylesheet" href="formulaires.css" />
-
-</head>
-
-<body>
-    <div id="container">
-
-        <div id="a1">
-            <h2>Informations du guide</h2>
-            <hr>
-        </div>
-        <form method="post" action="" enctype="multipart/form-data">
-            <div id="a2">
-                <label for="nom_guide">Nom du guide :</label>
-                <input type="text" name="nom du guide" required><br><br>
-            </div>
-            <div id="a3">
-                <label for="url_guide">URL du guide :</label>
-                <input type="url" name="url du guide" value="http:/numaguide.fr/" required><br><br>
-            </div>
-            <div id="a4">
-                <label for="auteur_guide">Auteur du guide :</label>
-                <input type="auteur" name="auteur du guide" required><br><br>
-            </div>
-            <div id="a5">
-                <label for="visibilite_guide">Indexer le guide sur les moteurs de recherches : </label>
-                <input type="checkbox" name="visibilité du guide" required><br><br>
-            </div>
-        </form>
-
-        <div id="a6">
-            <h2>Création du guide</h2>
-            <hr>
-        </div>
-
-        <h3 id="a7">Thèmes des couleurs</h3>
-
-        <form method="post" action="" enctype="multipart/form-data">
-            <button id="a8">Printemps</button>
-            <button id="a9">Jour</button>
-            <button id="a10">Nuit</button>
-        </form>
-
-        <h3 id="a11">Section 1</h3>
-        <button id="a12" class="repli">Replier la section</button>
-
-        <form method="post" action="" enctype="multipart/form-data">
-            <div id="a13">
-                <label for="nom_section1">Nom de la section : </label>
-                <input type="text" name="nom de la section" required>
-            </div>
-        </form>
-
-        <h4 id="a14">Slide 1</h4>
-        <form method="post" action="" enctype="multipart/form-data">
-            <textarea style="width: 700px; height: 200px; font-size: 15px;" placeholder="Entrer du texte (Max. 500)"
-                maxlength="500" wrap="soft" id="a15"></textarea>
-            <input type="file" name="parcourir" placeholder="Parcourir (PNG, JPEG, GIF..)" class="parcourir" id="a16">
-            <button id="a17" class="ajout_slide">Ajouter une slide</button>
-            <button id="a18">Aperçu</button>
-            <button id="a19">Valider</button>
-        </form>
-        <h3 id="a20">Sections 2</h3>
-        <button class="repli" id="a21">Replier la section</button>
-
-        <form method="post" action="" enctype="multipart/form-data">
-            <div id="a22">
-                <label for="nom_section1">Nom de la section : </label>
-                <input type="text" name="nom de la section" required>
-            </div>
-        </form>
-        <h4 id="a23">Slide 2</h4>
-        <input type="url" name="video" placeholder="URL de la vidéo" id="a24">
-        <h4 id="a25">Slide 2</h4>
-        <input type="url" name="video" placeholder="URL de la vidéo" id="a26">
-        <button id="a27"class="ajout_slide">Ajouter une slide</button>
-        <button id="a28" class="ajout_section">Ajouter une section</button>
-        <button id="a29">Aperçu</button>
-        <button id="a30">Valider</button>
-    </div>
-</body>
-
-</html> -->
