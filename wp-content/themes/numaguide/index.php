3f35c23368e01b3fad10c8991e8d123d9ec04bc5
FS <floralyne_segaud@yahoo.fr>
2022-05-20 13:04:27 +0200

    Conflit index.php

--- wp-content/themes/numaguide/index.php
+++ wp-content/themes/numaguide/index.php
@@ -3,15 +3,9 @@
 <main id="site-content">
 
     <?php
-<<<<<<< HEAD
-        $info_slide = array('slide20', 'slides/slide_20');
-        $slide = apply_filters('ng_article_pour_template', $info_slide);
-        apply_filters('ng_creer_guide','Slide 20 bis', $slide);
-=======
         $info_slide = array('slide24', 'slides/slide_24');
         $slide = apply_filters('ng_article_pour_template', $info_slide);
         apply_filters('ng_creer_guide','amelie big brain 12', $slide);
->>>>>>> develop
     ?>
 
 </main>
