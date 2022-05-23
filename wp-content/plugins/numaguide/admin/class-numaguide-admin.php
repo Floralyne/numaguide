a5f05fa1061323fd01e7beca9ba9f81dd0e56f13
Riham-Aida <99548412+Riham-Aida@users.noreply.github.com>
2022-05-23 16:32:26 +0200

    Update class-numaguide-admin.php

--- wp-content/plugins/numaguide/admin/class-numaguide-admin.php
+++ wp-content/plugins/numaguide/admin/class-numaguide-admin.php
@@ -121,6 +121,15 @@ class Numaguide_Admin
 			'ng_submenu_page_content'
 		);
 
+		add_submenu_page(
+			'numaguide',
+			'Numaguide | Test XML',
+			'Test XML',
+			'edit_posts',
+			'ng_xml',
+			'ng_submenu_xml_test'
+		);
+
 
 		function ng_admin_page_contents()
 		{
@@ -134,6 +143,11 @@ class Numaguide_Admin
 		function ng_submenu_page_content()
 		{
 			include_once( 'views/view_numaguide_form_creation.php' );
+		}
+
+		function ng_submenu_xml_test()
+		{
+			include_once( 'views/view_numaguide_test_xml.php' );
 		}
 	}
 
@@ -235,59 +249,3 @@ class Numaguide_Admin
     }
 
 }
-
-// // Ajout des pages dans la bar de menu administrateur
-// function ajout_menu_configuration($wp_admin_bar)
-// {
-//   $args = array(
-//     'id' => 'wp_numaguide',
-//     'title' => 'Numaguide',
-//     'href' => '/',
-//     'meta' => array(
-//       'class' => 'wp_config_numaguide',
-//       'title' => 'Search WPBeginner Tutorials'
-//     )
-//   );
-//   $wp_admin_bar->add_node($args);
-
-
-//   $args = array(
-//     'id' => 'wp_config_numaguide',
-//     'title' => 'Configuration',
-//     'href' => get_template_directory_uri() . '/page_config/form_configuration.php',
-//     'parent' => 'wp_numaguide',
-//     'meta' => array(
-//       'class' => 'wp_config_numaguide',
-//       'title' => 'Configuration'
-//     )
-//   );
-//   $wp_admin_bar->add_node($args);
-
-
-//   $args = array(
-//     'id' => 'wp_creation_numaguide',
-//     'title' => 'Création d\'un guide',
-//     'href' => get_template_directory_uri() . '/page_creation/form_creation.php',
-//     'parent' => 'wp_numaguide',
-//     'meta' => array(
-//       'class' => 'wp_creation_numaguide',
-//       'title' => 'Création d\'un guide'
-//     )
-//   );
-//   $wp_admin_bar->add_node($args);
-
-
-//   $args = array(
-//     'id' => 'wp_liste_numaguide',
-//     'title' => 'Liste des guides',
-//     'href' => get_template_directory_uri() . '/page_liste/form_liste.php',
-//     'parent' => 'wp_numaguide',
-//     'meta' => array(
-//       'class' => 'wp_liste_numaguide',
-//       'title' => 'Liste des guides'
-//     )
-//   );
-//   $wp_admin_bar->add_node($args);
-// }
-// add_action('admin_bar_menu', 'ajout_menu_configuration', 999);
-
