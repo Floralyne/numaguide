6e83cf2c0a61749bd923a1f4d723013e50b1ec19
Riham-Aida <99548412+Riham-Aida@users.noreply.github.com>
2022-05-23 19:46:32 +0200

    Update view_numaguide_test_xml.php

--- wp-content/plugins/numaguide/admin/views/view_numaguide_test_xml.php
+++ wp-content/plugins/numaguide/admin/views/view_numaguide_test_xml.php
@@ -14,14 +14,17 @@
 
             <div class="form-group pb-sm-2">
                 <label for="url">URL</label>
-                <input type="url" class="form-control" value="https://numaguide.fr/" required>
+                <input type="url" class="form-control" name='url' value="https://numaguide.fr/" required>
             </div>
             <input type=submit name="ok" value="Envoyer">
         </form>
+
 <?php
         if(isset($_POST['ok'])){
     $donnee_titre = $_POST['title'];
     $donnee_auteur = $_POST['author'];
+    $donnee_url = $_POST['url'];
+
 }
 ?>
 
@@ -31,51 +34,39 @@ $dom = new DOMDocument('1.0', 'utf-8');
 $dom->preserveWhiteSpace = false;
 $dom->formatOutput = true;
 
-//create the main tags, without values
-$donnees = $dom->createElement('donnees');
-$donnee_1 = $dom->createElement('donnee');
-// create some tags with values
-$nom_1 = $dom->createElement('nom', $donnee_titre);
-$domaine_1 = $dom->createElement('domaine', 'culture et ');
-$id_1 = $dom->createElement('id', '1');
+//creation des principaux elements, sans valeurs
+$all = $dom->createElement('metadonnees');
+$donnees = $dom->createElement('donneesObligatoires');
+$fichier = $dom->createElement('notes');
 
-//create  and append an attribute
-$attr_1 = $dom->createAttribute('version');
-$attr_1->value = '1.0';
-//append the attribute
-$id_1->appendChild($attr_1);
 
-//create the second tag donnee with different namespace
-$namespace = 'www.example.com/libraryns/1.0';
+//creation des elements avec valeurs
+$desc = $dom->createElement('description','fichier de métadonnees');
+$licence = $dom->createElement('licence', 'type de licence');
 
-//include the namespace prefix in the donnees tag
-$donnees->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:ns', $namespace);
-$donnee_2 = $dom->createElementNS($namespace,'ns:donnee');
-$nom_2 = $dom->createElementNS($namespace, 'ns:nom');
+$titre = $dom->createElement('title',$donnee_titre);
+$auteur = $dom->createElement('author', $donnee_auteur);
+$lien = $dom->createElement('URL', $donnee_url);
 
-//create a CDATA section (that is another DOMNode instance) and put it inside the name tag
-$nom_cdata = $dom->createCDATASection('Guide');
-$nom_2->appendChild($nom_cdata);
-$domaine_2 = $dom->createElementNS($namespace, 'ns:domaine', 'sciences humaines et sociales');
-$id_2 = $dom->createElementNS($namespace, 'ns:id', '2');
 
-//create the XML structure
-$donnees->appendChild($donnee_1);
-$donnee_1->appendChild($nom_1);
-$donnee_1->appendChild($domaine_1);
-$donnee_1->appendChild($id_1);
-$donnees->appendChild($donnee_2);
-$donnee_2->appendChild($nom_2);
-$donnee_2->appendChild($domaine_2);
-$donnee_2->appendChild($id_2);
+//creation de la structure xml
+$dom->appendChild($all);
+$all->appendChild($fichier);
+$all->appendChild($donnees);
 
-$dom->appendChild($donnees);
+$fichier->appendChild($desc);
+$fichier->appendChild($licence);
 
-//saveXML() method returns the XML in a String
+$donnees->appendChild($titre);
+$donnees->appendChild($lien);
+$donnees->appendChild($auteur);
+
+
+//Renvoi du XML en chaine de caracteres
 $tag = $dom->saveXML($dom->documentElement);
 
 if(isset($_POST['ok'])){
-    echo htmlspecialchars($tag);
+    echo htmlspecialchars ($tag);
 }
 ?>
 <div>
