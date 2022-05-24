35f84ea1ec1c63eba068cd893e8e087cd5203272
FS <floralyne_segaud@yahoo.fr>
2022-05-24 14:19:39 +0200

    Debug video
    Pas fini

--- wp-content/themes/numaguide/index.php
+++ wp-content/themes/numaguide/index.php
@@ -5,7 +5,7 @@
     <?php
         $info_slide = array('slide27', 'slides/slide_27');
         $slide = apply_filters('ng_article_pour_template', $info_slide);
-        apply_filters('ng_creer_guide','Mission Debug 2', $slide);
+        apply_filters('ng_creer_guide','Mission Debug 7', $slide);
     ?>
 
 </main>
