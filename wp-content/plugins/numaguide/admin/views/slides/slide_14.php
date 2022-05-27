dc62a7343f5acabf02d16c84781082c0b66de100
FS <floralyne_segaud@yahoo.fr>
2022-05-27 13:48:35 +0200

    Fonction de création d'une page avec remplissage via un formulaire

---
+++ wp-content/plugins/numaguide/admin/views/slides/slide_14.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Template Name: Slide 14
+ * Template Post Type: post, page
+ */
+
+?>
+<?php get_header() ?>
+
+<?php
+    $post_tab = $args['article'];
+    $post = $post_tab[0];
+    $blocks = parse_blocks($post->post_content);
+
+    $audios = array();
+    foreach ($blocks as $block) {
+        if ($block['blockName'] == 'core/audio') {
+            $audio = substr($block['innerContent'][0], 52);
+            $audio = substr($audio, 0, -19);
+            $audios[] = $audio;
+        }
+    }
+?>
+
+    <section id="slide_14">
+        <div id="slide_14_c" class="container-fluid">
+            <div id="slide_14_r" class="row vh-100">
+                <div id="slide_14_son1" class="row text-center mt-auto mb-auto p-5 embed-responsive ml-auto mr-auto">
+                    <audio controls class="embed-responsive-item" id="son1">
+                    <source src=<?=$audios[0]?>>
+                    </audio>
+                    <div id="slide_14_son_legende1" class="text-center p-2"><?php
+                        $post_tab = $args['article'];
+                        $post = $post_tab[0];
+                        $blocks = parse_blocks($post->post_content);
+
+                        $paragraphes = array();
+                        foreach ($blocks as $block) {
+                            if ($block['blockName'] == 'core/paragraph') {
+                                $paragraphes[] = $block;
+                            }
+                        }
+                        echo render_block($paragraphes[0]);
+                    ?></div>
+                </div>
+                <div id="slide_14_son2" class="row text-center mt-auto mb-auto p-5 embed-responsive ml-auto mr-auto">
+                    <audio controls class="embed-responsive-item" id="son2">
+                    <source src=<?=$audios[1]?>>
+                    </audio>
+                    <div id="slide_14_son_legende2" class="text-center p-2"><?php
+                        echo render_block($paragraphes[1]);
+                    ?></div>
+                </div>
+            </div>
+        </div>
+        </div>
+</section>
--- wp-content/plugins/numaguide/admin/views/view_numaguide_test_guide.php
+++ wp-content/plugins/numaguide/admin/views/view_numaguide_test_guide.php
@@ -3,6 +3,11 @@
 
         <form name="form" action="" method="post">
             <div class="form-group pb-sm-2">
+                <label for="nomGuide">Choisissez le titre du guide</label>
+                <input type="text" class="form-control" id="nomGuide" name="nomGuide">
+            </div>
+
+            <div class="form-group pb-sm-2">
                 <label for="slide1">Choisir une slide avec deux textes</label>
                 <select name="slide1">
                     <option value="slide10" selected>Slide 10</option>
@@ -15,7 +20,7 @@
                 <input type="text" class="form-control" name="slide1-2">
             </div>
             
-            <div class="form-group pb-sm-2">
+            <!-- <div class="form-group pb-sm-2">
                 <label for="slide7">Slide avec 7 textes</label>
                 <input type="text" id="slide7" name="slide7" value="slide7" disabled>
                 <input type="text" class="form-control" name="slide13-1">
@@ -25,7 +30,7 @@
                 <input type="text" class="form-control" name="slide13-5">
                 <input type="text" class="form-control" name="slide13-6">
                 <input type="text" class="form-control" name="slide13-7">
-            </div>
+            </div> -->
 
             <div class="form-group pb-sm-2">
                 <label for="slide11">Slide avec uns textes</label>
@@ -41,14 +46,14 @@
                 <input type="text" class="form-control" name="slide8-3">
             </div>
 
-            <div class="form-group pb-sm-2">
+            <!-- <div class="form-group pb-sm-2">
                 <label for="slide13">Slide avec 4 texte</label>
                 <input type="text" id="slide13" name="slide13" value="slide13" disabled>
                 <input type="text" class="form-control" name="slide13-1">
                 <input type="text" class="form-control" name="slide13-2">
                 <input type="text" class="form-control" name="slide13-3">
                 <input type="text" class="form-control" name="slide13-4">
-            </div>
+            </div> -->
 
             <input type=submit name="ok" value="Créer le mini guide">
         </form>
@@ -57,36 +62,46 @@
 
 <?php
     if(isset($_POST['ok'])){
-        // $ng_slide1_nom = $_POST['slide1'];
-        // $ng_slide1_value1 = $_POST['slide1-1'];
-        // $ng_slide1_value2 = $_POST['slide1-2'];
+        $ng_guide_nom = $_POST['nomGuide'];
 
-        // $ng_content = '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
-        //                 $ng_slide1_value1 . '<!-- /wp:paragraph -->' . 
-        //                 '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
-        //                 $ng_slide1_value2 . '<!-- /wp:paragraph -->';
+        $ng_slide1_nom = $_POST['slide1'];
+        $ng_slide1_value1 = $_POST['slide1-1'];
+        $ng_slide1_value2 = $_POST['slide1-2'];
+
+        $ng_content = '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
+                        $ng_slide1_value1 . '<!-- /wp:paragraph -->' . 
+                        '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
+                        $ng_slide1_value2 . '<!-- /wp:paragraph -->';
 
 
-        // if($ng_slide1_nom == 'slide10') {
-        //     $num_cat = 22;
-        // }if($ng_slide1_nom == 'slide12') {
-        //     $num_cat = 16;
-        // }if($ng_slide1_nom == 'slide14') {
-        //     $num_cat = 17;
-        // }if($ng_slide1_nom == 'slide17') {
-        //     $num_cat = 13;
-        // }if($ng_slide1_nom == 'slide18') {
-        //     $num_cat =14;
-        // }
+        if($ng_slide1_nom == 'slide10') {
+            $num_cat = 22;
+            $nom_slide = 'slide_4';
+        }if($ng_slide1_nom == 'slide12') {
+            $num_cat = 16;
+            $nom_slide = 'slide_3';
+        }if($ng_slide1_nom == 'slide14') {
+            $num_cat = 17;
+            $nom_slide = 'slide_5';
+        }if($ng_slide1_nom == 'slide17') {
+            $num_cat = 13;
+            $nom_slide = 'slide_7';
+        }if($ng_slide1_nom == 'slide18') {
+            $num_cat =14;
+            $nom_slide = 'slide_8';
+        }
         
-        // $ng_info_article = array(
-        //     'post_content'  => $ng_content,
-        //     'post_category' => array( $num_cat ),
-        //     'tags_input' => array('1'),
-        //     'post_type' => 'post'
-        // );
+        $ng_info_article = array(
+            'post_content'  => $ng_content,
+            'post_category' => array( $num_cat ),
+            'tags_input' => array('1'),
+            'post_type' => 'post'
+        );
             
-        // wp_insert_post($ng_info_article);
+        wp_insert_post($ng_info_article);
+
+        $info_slide = array($num_cat, 'views/slides/' . $nom_slide . '.php');
+        $slide = $slide . apply_filters('ng_article_pour_template', $info_slide);
 
         // $ng_slide7_nom = $_POST['slide7'];
         // $ng_slide7_value1 = $_POST['slide7-1'];
@@ -135,56 +150,62 @@
         );
             
         wp_insert_post($ng_info_article);
+        
+        $info_slide = array('slide6', 'views/slides/slide_6.php');
+        $slide = $slide . apply_filters('ng_article_pour_template', $info_slide);
+        
+        $ng_slide8_nom = $_POST['slide8'];
+        $ng_slide8_value1 = $_POST['slide8-1'];
+        $ng_slide8_value2 = $_POST['slide8-2'];
+        $ng_slide8_value3 = $_POST['slide8-3'];
 
-        $info_slide = array('slide11', plugin_dir_path(__FILE__) . 'public/views/slide_11.php');
-        $slide = apply_filters('ng_article_pour_template', $info_slide);
-
-        // $ng_slide8_nom = $_POST['slide8'];
-        // $ng_slide8_value1 = $_POST['slide8-1'];
-        // $ng_slide8_value2 = $_POST['slide8-2'];
-        // $ng_slide8_value3 = $_POST['slide8-3'];
-
-        // $ng_content = '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
-        //                 $ng_slide8_value1 . '<!-- /wp:paragraph -->' .
-        //                 '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
-        //                 $ng_slide8_value2 . '<!-- /wp:paragraph -->' . 
-        //                 '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
-        //                 $ng_slide8_value3 . '<!-- /wp:paragraph -->';
+        $ng_content = '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
+                        $ng_slide8_value1 . '<!-- /wp:paragraph -->' .
+                        '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
+                        $ng_slide8_value2 . '<!-- /wp:paragraph -->' . 
+                        '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
+                        $ng_slide8_value3 . '<!-- /wp:paragraph -->';
                         
-        // $ng_info_article = array(
-        //     'post_content'  => $ng_content,
-        //     'post_category' => array( 11 ),
-        //     'tags_input' => array('1'),
-        //     'post_type' => 'post'
-        // );
+        $ng_info_article = array(
+            'post_content'  => $ng_content,
+            'post_category' => array( 11 ),
+            'tags_input' => array('1'),
+            'post_type' => 'post'
+        );
 
-        // wp_insert_post($ng_info_article);
+        wp_insert_post($ng_info_article);
 
-        // $ng_slide13_nom = $_POST['slide13'];
-        // $ng_slide13_value1 = $_POST['slide13-1'];
-        // $ng_slide13_value2 = $_POST['slide13-2'];
-        // $ng_slide13_value3 = $_POST['slide13-3'];
-        // $ng_slide13_value4 = $_POST['slide13-4'];
+        $info_slide = array('slide1', 'views/slides/slide_1.php');
+        $slide = $slide . apply_filters('ng_article_pour_template', $info_slide);
 
-        // $ng_content = '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
-        //                 $ng_slide13_value1 . '<!-- /wp:paragraph -->' .
-        //                 '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
-        //                 $ng_slide8_value2 . '<!-- /wp:paragraph -->' . 
-        //                 '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
-        //                 $ng_slide8_value3 . '<!-- /wp:paragraph -->' .
-        //                 '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
-        //                 $ng_slide8_value3 . '<!-- /wp:paragraph -->';
+        $ng_slide13_nom = $_POST['slide13'];
+        $ng_slide13_value1 = $_POST['slide13-1'];
+        $ng_slide13_value2 = $_POST['slide13-2'];
+        $ng_slide13_value3 = $_POST['slide13-3'];
+        $ng_slide13_value4 = $_POST['slide13-4'];
 
-        // $ng_info_article = array(
-        //     'post_content'  => $ng_content,
-        //     'post_category' => array( 15 ),
-        //     'tags_input' => array('1'),
-        //     'post_type' => 'post'
-        // );
+        $ng_content = '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
+                        $ng_slide13_value1 . '<!-- /wp:paragraph -->' .
+                        '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
+                        $ng_slide8_value2 . '<!-- /wp:paragraph -->' . 
+                        '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
+                        $ng_slide8_value3 . '<!-- /wp:paragraph -->' .
+                        '<!-- wp:paragraph {"placeholder":"Post Paragraph"} -->' . 
+                        $ng_slide8_value3 . '<!-- /wp:paragraph -->';
 
-        // wp_insert_post($ng_info_article);
+        $ng_info_article = array(
+            'post_content'  => $ng_content,
+            'post_category' => array( 15 ),
+            'tags_input' => array('1'),
+            'post_type' => 'post'
+        );
 
+        wp_insert_post($ng_info_article);
 
+        $info_slide = array('slide2', 'views/slides/slide_2.php');
+        $slide = $slide . apply_filters('ng_article_pour_template', $info_slide);
+
+        apply_filters('ng_creer_guide', $ng_guide_nom, $slide);
     }
 ?>
 
