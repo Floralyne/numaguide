d1adc07de798f3dcf59c73c7c875b7d122d0175c
ameliegauthier <99548225+ameliegauthier@users.noreply.github.com>
2022-05-30 16:28:34 +0200

    taxonomie

--- wp-content/plugins/numaguide/admin/views/view_numaguide_test_guide.php
+++ wp-content/plugins/numaguide/admin/views/view_numaguide_test_guide.php
@@ -70,7 +70,32 @@
           }
           </style>";
         $ng_guide_nom = $_POST['nomGuide'];
-
+        $enleveaccent_array = array('Š' => 'S', 'š' => 's', 'Ž' => 'Z', 'ž' => 'z', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E',
+            'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U',
+            'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c',
+            'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o',
+            'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ý' => 'y', 'þ' => 'b', 'ÿ' => 'y');
+        
+        $ng_guide_nom = strtr($ng_guide_nom, $enleveaccent_array);
+        $ng_guide_nom = str_replace('\'', '', trim($ng_guide_nom));
+        $ng_guide_nom = preg_replace('/[^A-Za-z0-9-]/', '', $ng_guide_nom);
+        $ng_guide_nom = preg_replace('/-+/', '-', $ng_guide_nom);
+        $ng_guide_nom = substr($ng_guide_nom, 0, 20);
+        
+        register_taxonomy($ng_guide_nom, "post", array(
+            "hierarchical" => true,
+	    "label" => "Guide",
+	"singular_label" => "Guide",
+	'query_var' => true,
+	'rewrite' => array( 'slug' => 'tutorial', 'with_front' => false ),
+	'public' => true,
+	'show_ui' => true,
+	'show_tagcloud' => true,
+	'_builtin' => false,
+	'show_in_nav_menus' => false
+        ));
+        die("finito");
+        
         //Slide avec trois textes
         if ($_POST['slide1-1'] !== ''&$_POST['slide1-2'] !== ''&$_POST['slide1-3'] !== '') {
             $ng_slide1_nom = $_POST['slide1'];
@@ -136,6 +161,7 @@
             $ng_info_article = array(
                 'post_content' => $ng_content,
                 'post_category' => array($num_cat),
+                // 'post_taxonomy' => array()
                 'tags_input' => array('1'),
                 'post_type' => 'post',
             );
