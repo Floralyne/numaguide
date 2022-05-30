36ba02c10f9dd788bf5f619443c297a1f9ef7e03
ameliegauthier <99548225+ameliegauthier@users.noreply.github.com>
2022-05-30 11:32:35 +0200

    page d'accueil

---
+++ wp-content/themes/numaguide/accueil.php
@@ -0,0 +1,32 @@
+<?php get_header() ?>
+
+<style>
+    html {
+        margin-top: 0 !important;
+    }
+
+    @media screen and (max-width: 782px) {
+        html {
+            margin-top: 0 !important;
+        }
+    }
+</style>
+
+<section class="accueil">
+        <div class="containeraccueil">
+            <div class="messageaccueil">Comment créer un mini-guide :<br><br>
+                - Aller dans l'onglet Numaguide puis sur 'Test Guide'<br>
+                - Renseigner le titre du Guide<br>
+                - Pour la Slide 1, renseigner un titre, un sous-titre et un auteur<br>
+                - Choisir le modèle de slide souhaité et entrer les données<br>
+                - Cliquer sur créer le mini-guide<br>
+                - Accéder au guide via le lien<br>
+                <br>
+            <br>
+            Comment générer les métadonnées :<br><br>
+            - Aller dans l'onglet Numaguide puis sur 'Test XML'<br>
+            - Remplir les champs<br>
+            - Cliquer sur envoyer<br>
+            </div>
+        </div>
+</section>
