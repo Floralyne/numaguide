3bd2b7626865201d0d9dd568cff2d43f6538b170
ameliegauthier <99548225+ameliegauthier@users.noreply.github.com>
2022-06-07 11:32:54 +0200

    Slide 16 Vidéo

--- wp-content/plugins/numaguide/admin/views/slides/slide_16.php
+++ wp-content/plugins/numaguide/admin/views/slides/slide_16.php
@@ -33,10 +33,10 @@
  <section id="slide_16">
         <div id="slide_16_c" class="container-fluid">
             <div id="slide_16_r" class="vh-100">
-                <div id="slide_16_div1" class="row text-center mt-auto mb-auto pt-5 justify-content-center">
+                <div id="slide_16_div1" class="row text-center mt-auto mb-auto pt-5 mr-5 ml-5 justify-content-center">
                         <iframe src=<?=$video?> class="mt-auto mb-auto" id="slide_16_video"></iframe>
                 </div>
-                <div id="slide_16_titre" class="row text-center p-2 justify-content-center">
+                <div id="slide_16_titre" class="row text-center m-3 justify-content-center">
                         <?php
                         $post_tab = $args['article'];
                         $post = $post_tab[0];
@@ -51,7 +51,7 @@
                         echo render_block($paragraphes[0]);
                     ?>
                 </div>
-                <div class="row text-justify mt-auto mb-auto p-5">
+                <div id="slide_16_texte_div" class="row text-justify mt-auto mb-auto p-5">
                     <div id="slide_16_texte2" class="slide_16_texte">
                     <?php
                         echo render_block($paragraphes[1]);
