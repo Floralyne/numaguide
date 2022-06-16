d5b4152a823cba3d255de1f6a0febc15cb09bddb
ameliegauthier <99548225+ameliegauthier@users.noreply.github.com>
2022-06-16 22:12:20 +0200

    Formulaire liste des guides

--- wp-content/plugins/numaguide/admin/views/view_numaguide_liste_guides.php
+++ wp-content/plugins/numaguide/admin/views/view_numaguide_liste_guides.php
@@ -2,17 +2,27 @@
 
     <div class="titreForm">Liste des guides</div>
     <hr>
-    <nav class="navbar">
 
-        <form class="form">
-            <div class="row">
-                <div class="col"> <input class="" type="search" placeholder="Rechercher un guide" aria-label="Search" disabled>
-                </div>
-                <div class="col"> <button class="boutonForm boutonAction" type="submit" disabled>Search</button>
-                </div>
-            </div>
+    <div id="blocliste" class="container-fluid mb-3 mt-4">
+        <div class="row">
+            <div class="col text-center mt-2">Titre</div>
+            <div class="col text-center mt-2" id="listeauteur">Auteur</div>
+            <div class="col text-center mt-2">Date</div>
+        </div>
+    </div>
 
-    </nav>
+    <div class="form-group row">
+        <div class="col">
+            <input class="boutonForm boutonAjouter" type="submit" value="Consulter" />
+        </div>
+        <div class="col">
+            <input class="boutonForm boutonAction" type="submit" value="Modifier" />
 
-    </form>
+        </div>
+        <div class="col">
+            <input class="boutonForm boutonSupprimer" type="submit" value="Supprimer" />
+
+        </div>
+    </div>
+
 </div>
