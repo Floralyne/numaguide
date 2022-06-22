9ef527067ff14a5006634b944b35242dce4ac6e3
ameliegauthier <99548225+ameliegauthier@users.noreply.github.com>
2022-06-22 11:29:26 +0200

    Fete de la science

---
+++ wp-content/themes/numaguide/front-page.php
@@ -0,0 +1,21 @@
+<?php get_header() ?>
+
+<style>
+    html {
+        margin-top: 0 !important;
+    }
+
+    @media screen and (max-width: 782px) {
+        html {
+            margin-top: 0 !important;
+        }
+    }
+</style>
+
+<section class="accueil">
+        <div class="containeraccueil">
+        <div class="img-fluid">
+            <img src="<?= get_template_directory_uri() . '/assets/images/logotype_numaguide.png'; ?>">
+        </div>
+        </div>
+</section>
